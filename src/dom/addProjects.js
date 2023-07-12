3d8834b7634051a378ef8c729a5ac8015c285982
Abdul Rehman <abdulrehmang571@gmail.com>
2023-07-12 14:42:42 +0500

    modified addProject.js added cancel button

--- src/dom/addProjects.js
+++ src/dom/addProjects.js
@@ -7,22 +7,38 @@ export default function addProject() {
   const addProjectButton = document.querySelector('.addProjectButton');
   const projectInputDiv = document.querySelector('.projectInputDiv');
   const submitProjectButton = document.querySelector('.submitProjectButton');
-  addProjectButton.addEventListener('click', () => {
-    projectInputDiv.style.display = 'block';
-    submitProjectButton.addEventListener('click', () => {
-      const projectInput = document.querySelector('.projectInput');
-      const isValid = projectInput.checkValidity();
-      if (!isValid) projectInput.reportValidity();
-      else {
-        projectInputDiv.style.display = 'none';
-        createProject(projectInput.value);
-        projectInput.value = '';
-        updateProjectView();
+  const cancelButton = document.querySelector('.cancelProjectInput');
 
-        // setting event listeners
-        getSelectedProjectName();
-        updateProjectTodos();
-      }
-    });
+  addProjectButton.addEventListener('click', () => {
+    addProjectButton.style.display = 'none';
+    projectInputDiv.style.display = 'block';
+
+    submitProjectButton.addEventListener('click', submit);
+
+    cancelButton.addEventListener('click', reset);
   });
+}
+
+function submit() {
+  const projectInput = document.querySelector('.projectInput');
+
+  const isValid = projectInput.checkValidity();
+  if (!isValid) projectInput.reportValidity();
+  else {
+    createProject(projectInput.value);
+
+    reset();
+
+    updateProjectView();
+
+    // setting event listeners
+    getSelectedProjectName();
+    updateProjectTodos();
+  }
+}
+
+function reset() {
+  document.querySelector('.addProjectButton').style.display = 'block';
+  document.querySelector('.projectInputDiv').style.display = 'none';
+  document.querySelector('.projectInput').value = '';
 }
