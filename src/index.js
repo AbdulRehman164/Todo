7f4f5ca71cbf2e37cd4487ec376b73a23a6c5b8a
Abdul Rehman <abdulrehmang571@gmail.com>
2023-07-17 16:27:24 +0500

    fixed the addtodo button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import { createProject } from './logic/project';
 import renderView from './dom/renderView';
+import { createTodo } from './logic/todo';
 
+const data = {
+  title: 'Do HomeWork',
+  description:
+    'Do HomeWork do this do that lorem ipusm this that what the  is going on what the  is this',
+  dueDate: '2023-12-12',
+  priority: 'low',
+  isChecked: false,
+};
+createTodo(data, 'Inbox');
+createTodo(data, 'Inbox');
+createTodo(data, 'Inbox');
+createTodo(data, 'Inbox');
 createProject('Inbox');
 createProject('Chores');
 renderView();
